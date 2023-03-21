25e18ba2be4918c15a4343005d7e08c8f509f506
CobzacD <75493180+CobzacD@users.noreply.github.com>
2023-03-22 00:32:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wsqeor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wsqeor)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+// import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
+// import { Component, Input } from '@angular/core';
+// import { Product } from '../products';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
